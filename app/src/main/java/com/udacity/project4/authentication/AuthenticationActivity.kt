778235378dc8ae7e977effbda30b04b062ff1266
samueljuma <samueljuma1024@gmail.com>
2024-04-14 12:44:56 +0300

    add Login Button to Authenticator layout

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -2,7 +2,9 @@ package com.udacity.project4.authentication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,9 +12,12 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAuthenticationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+
         // TODO: Implement the create account and sign in using FirebaseUI,
         //  use sign in using email and sign in using Google
 
